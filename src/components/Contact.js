9b467049e2c4ed04f026ccfdcebfb829fefe7053
JD Martinez <focus4ursoul@gmail.com>
2021-02-12 20:07:22 -0800

    base html for contact page - formSpree integration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,15 @@
-
+import Form from "../components/SubComp/Form";
 
 export default function Contact() {
     return (
         <section className="contactContainer">
-            Contact Container
+            <div className="contactWrapper">
+                <div className="partingText">
+                    <h2>LET'S CONNECT!</h2>
+                    <p>Complete this form and I'll be in touch with you soon.</p>
+                </div>
+                <Form />
+            </div>
         </section>
     )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
-import '../../styles/globals.css'
+import "../../styles/globals.css"
 import "../../styles/main.css"
 import "../../styles/listings.css"
+import "../../styles/contact.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

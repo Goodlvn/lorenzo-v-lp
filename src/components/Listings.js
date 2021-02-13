8a26440992cd7ddcee2650abe43f768a9f3a5f76
JD Martinez <focus4ursoul@gmail.com>
2021-02-12 18:36:17 -0800

    add base structure and style for listings

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -3,7 +3,36 @@
 export default function Listings() {
     return (
         <section className="listingsContainer">
-            Listing Section
+            <div className="forSale listings">
+                <h3>FOR SALE</h3>
+                <div id="prop1" className="propImage">
+                    <div className="propInfo">
+                        <h3>House #1</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div id="prop2" className="propImage">
+                    <div className="propInfo">
+                        <h3>House #3</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="recentlySold listings">
+                <h3>RECENTLY SOLD</h3>
+                <div id="prop3" className="propImage">
+                    <div className="propInfo">
+                        <h3>House #2</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div id="prop4" className="propImage">
+                    <div className="propInfo">
+                        <h3>House #4</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }

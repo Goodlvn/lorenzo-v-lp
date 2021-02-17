f4b2d4a654f4c1e17655bc5d69298d84cbb93b19
JD Martinez <focus4ursoul@gmail.com>
2021-02-16 20:47:40 -0800

    update new design - sans responsivness

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,17 +11,21 @@ export default function Main() {
 
             <div className="infoContainer">
                 <div className="infoMain">
-                    <h3>LORENZO VASQUEZ</h3>
-                    <p>DRE#   212504941</p>
+                    <div className="infoWrapper">
+                        <h3>LORENZO VASQUEZ</h3>
+                        <p>DRE#   212504941</p>
 
-                    <p class="firstBlue">P 559.667.7612</p>
-                    <p class="firstBlue">E lorenzovasquez@gmail.com</p>
+                        <p class="firstBlue">P 559.667.7612</p>
+                        <p class="firstBlue">E lorenzovasquez@gmail.com</p>
 
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate eros enim, varius tempor lorem vehicula consequat. Mauris lacus ipsum.
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate eros enim, varius tempor lorem vehicula consequat. Mauris lacus ipsum.
                     </p>
+                    </div>
                 </div>
-                <img id="lorenzo" src="/images/lv-mainshot.png" alt="Lorenzo Vasquez" />
+                <div class="lorenzoImage">
+                    <img id="lorenzo" src="/images/lv-shot.jpg" alt=" Lorenzo Vasquez" />
+                </div>
             </div>
 
             <div id="angleDivWhite"></div>

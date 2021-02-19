a351ca5b5db5e476d774514581399fdb723a2503
JD Martinez <focus4ursoul@gmail.com>
2021-02-18 20:48:22 -0800

    responsive contact validation styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -97,10 +97,10 @@ export default function Contact() {
     return (
         <section className="contactContainer">
             <div className="contactWrapper">
-                <div className="contactText">
+                {status === "" && <div className="contactText">
                     <h2>LET'S CONNECT!</h2>
                     <p>Complete this form and I'll be in touch with you soon.</p>
-                </div>
+                </div>}
                 <FormHook
                     status={status}
                     invalidName={invalidName}

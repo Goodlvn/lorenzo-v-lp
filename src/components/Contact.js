6ff607d3f4eb9431bddcc773609249c631ac86b5
JD Martinez <focus4ursoul@gmail.com>
2021-02-18 20:35:12 -0800

    move state managment up by one component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,99 @@
 import Form from "../components/SubComp/Form";
 import FormHook from "../components/SubComp/FormHook";
+import { useState } from "react"
 
 export default function Contact() {
+
+    const [contactState, setContactState] = useState({
+        status: "",
+        name: "",
+        phone: "",
+        email: "",
+    })
+
+    const [errorState, setErrorState] = useState({
+        invalidName: false,
+        invalidEmail: false,
+        invalidNumber: false
+    })
+
+    const { name, phone, email, status } = contactState;
+    const { invalidName, invalidEmail, invalidNumber } = errorState;
+
+    const handleChange = e => {
+        const { value, name } = e.target;
+        const regexNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+        const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+        setContactState({ ...contactState, [name]: value });
+
+        if (name === "name") {
+            setErrorState({ ...errorState, invalidName: false });
+        }
+
+        if (name === "phone" && regexNumber.test(value)) {
+
+            setErrorState({ ...errorState, invalidNumber: false });
+        }
+
+        if (name === "email" && regexEmail.test(value)) {
+            setErrorState({ ...errorState, invalidEmail: false });
+        }
+    }
+
+    const submitForm = (ev) => {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+
+
+        if (name === "" && phone === "" && email === "") {
+
+            setErrorState({ invalidName: true, invalidEmail: true, invalidNumber: true });
+
+            return
+
+        } else if (name === "" && phone !== "" && email !== "") {
+
+            setErrorState({ invalidName: true, invalidNumber: false, invalidEmail: false });
+            return
+        } else if (name === "" && phone === "" && email !== "") {
+
+            setErrorState({ invalidName: true, invalidNumber: true, invalidEmail: false });
+            return
+        } else if (name !== "" && phone === "" && email !== "") {
+
+            setErrorState({ invalidName: false, invalidNumber: true, invalidEmail: false });
+            return
+        } else if (name !== "" && phone === "" && email === "") {
+
+            setErrorState({ invalidName: false, invalidNumber: true, invalidEmail: true });
+            return
+        } else if (name !== "" && phone !== "" && email === "") {
+
+            setErrorState({ invalidName: false, invalidNumber: false, invalidEmail: true });
+            return
+        } else if (name === "" && phone !== "" && email === "") {
+
+            setErrorState({ invalidName: true, invalidNumber: false, invalidEmail: true });
+            return
+        } else {
+            xhr.open(form.method, form.action);
+            xhr.setRequestHeader("Accept", "application/json");
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState !== XMLHttpRequest.DONE) return;
+                if (xhr.status === 200) {
+                    form.reset();
+                    setContactState({ status: "SUCCESS" });
+                } else {
+                    setContactState({ status: "ERROR" });
+                }
+            };
+            xhr.send(data);
+        }
+    }
+
     return (
         <section className="contactContainer">
             <div className="contactWrapper">
@@ -9,7 +101,14 @@ export default function Contact() {
                     <h2>LET'S CONNECT!</h2>
                     <p>Complete this form and I'll be in touch with you soon.</p>
                 </div>
-                <FormHook />
+                <FormHook
+                    status={status}
+                    invalidName={invalidName}
+                    invalidEmail={invalidEmail}
+                    invalidNumber={invalidNumber}
+                    handleChange={handleChange}
+                    submitForm={submitForm}
+                />
             </div>
         </section>
     )
--- src/components/SubComp/FormHook.js
+++ src/components/SubComp/FormHook.js
@@ -1,100 +1,10 @@
 import React, { useState } from "react";
 
-export default function FormHook() {
-
-    const [contactState, setContactState] = useState({
-        status: "",
-        name: "",
-        phone: "",
-        email: "",
-    })
-
-    const [errorState, setErrorState] = useState({
-        invalidName: false,
-        invalidEmail: false,
-        invalidNumber: false
-    })
-
-    const { name, phone, email } = contactState;
-    const { invalidName, invalidEmail, invalidNumber } = errorState;
-
-    const handleChange = e => {
-        const { value, name } = e.target;
-        const regexNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
-        const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-
-        setContactState({ ...contactState, [name]: value });
-
-        if (name === "name") {
-            setErrorState({ ...errorState, invalidName: false });
-        }
-
-        if (name === "phone" && regexNumber.test(value)) {
-
-            setErrorState({ ...errorState, invalidNumber: false });
-        }
-
-        if (name === "email" && regexEmail.test(value)) {
-            setErrorState({ ...errorState, invalidEmail: false });
-        }
-    }
-
-    const submitForm = (ev) => {
-        ev.preventDefault();
-        const form = ev.target;
-        const data = new FormData(form);
-        const xhr = new XMLHttpRequest();
-
-
-        if (name === "" && phone === "" && email === "") {
-
-            setErrorState({ invalidName: true, invalidEmail: true, invalidNumber: true });
-
-            return
-
-        } else if (name === "" && phone !== "" && email !== "") {
-
-            setErrorState({ invalidName: true, invalidNumber: false, invalidEmail: false });
-            return
-        } else if (name === "" && phone === "" && email !== "") {
-
-            setErrorState({ invalidName: true, invalidNumber: true, invalidEmail: false });
-            return
-        } else if (name !== "" && phone === "" && email !== "") {
-
-            setErrorState({ invalidName: false, invalidNumber: true, invalidEmail: false });
-            return
-        } else if (name !== "" && phone === "" && email === "") {
-
-            setErrorState({ invalidName: false, invalidNumber: true, invalidEmail: true });
-            return
-        } else if (name !== "" && phone !== "" && email === "") {
-
-            setErrorState({ invalidName: false, invalidNumber: false, invalidEmail: true });
-            return
-        } else if (name === "" && phone !== "" && email === "") {
-
-            setErrorState({ invalidName: true, invalidNumber: false, invalidEmail: true });
-            return
-        } else {
-            xhr.open(form.method, form.action);
-            xhr.setRequestHeader("Accept", "application/json");
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState !== XMLHttpRequest.DONE) return;
-                if (xhr.status === 200) {
-                    form.reset();
-                    setContactState({ status: "SUCCESS" });
-                } else {
-                    setContactState({ status: "ERROR" });
-                }
-            };
-            xhr.send(data);
-        }
-    }
+export default function FormHook({ status, invalidName, invalidEmail, invalidNumber, submitForm, handleChange }) {
 
     return (
         <>
-            {contactState.status === "" ? <form
+            {status === "" ? <form
                 className="contactForm"
                 onSubmit={submitForm}
                 action="https://formspree.io/f/xgepbega"
@@ -123,8 +33,8 @@ export default function FormHook() {
                     </div>
                 </div>
 
-                {contactState.status === "SUCCESS" ? <p>Thanks!</p> : <button className="contactSubmitBtn" >SUBMIT</button>}
-                {contactState.status === "ERROR" && <p>Ooops! There was an error.</p>}
+                {status === "SUCCESS" ? <p>Thanks!</p> : <button className="contactSubmitBtn" >SUBMIT</button>}
+                {status === "ERROR" && <p>Ooops! There was an error.</p>}
             </form>
 
                 :

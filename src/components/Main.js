5b78a7c85667d28c2629cde01d0edf0573e2a9ae
JD Martinez <focus4ursoul@gmail.com>
2021-02-19 18:25:24 -0800

    + react transition group & fade in banner

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,46 @@
-
+import { CSSTransition } from "react-transition-group";
 
 export default function Main() {
     return (
         <main className="mainContainer">
             <div id="angleDivBeige">
-                <img id="logoHeader" src="/images/lv-logo.png" alt="lorenzo-v logo" />
+                <CSSTransition
+                    in={true}
+                    appear={true}
+                    timeout={10000}
+                    classNames="fade"
+                >
+                    <img id="logoHeader" class="fade-in" src="/images/lv-logo.png" alt="lorenzo-v logo" />
+                </CSSTransition>
             </div>
 
 
 
-            <div className="infoContainer">
-                <div className="infoMain">
-                    <div className="infoWrapper">
-                        <h3>LORENZO VASQUEZ</h3>
-                        <p>DRE#   212504941</p>
+            <CSSTransition
+                in={true}
+                appear={true}
+                timeout={10000}
+                classNames="fade"
+            >
+                <div className="infoContainer">
+                    <div className="infoMain">
+                        <div className="infoWrapper">
+                            <h3>LORENZO VASQUEZ</h3>
+                            <p>DRE#   212504941</p>
 
-                        <p className="firstBlue">P 559.667.7612</p>
-                        <p className="firstBlue">E lorenzovasquez@gmail.com</p>
+                            <p className="firstBlue">P 559.667.7612</p>
+                            <p className="firstBlue">E lorenzovasquez@gmail.com</p>
 
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate eros enim, varius tempor lorem vehicula consequat. Mauris lacus ipsum.
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate eros enim, varius tempor lorem vehicula consequat. Mauris lacus ipsum.
                     </p>
+                        </div>
+                    </div>
+                    <div className="lorenzoImage">
+                        <img id="lorenzo" src="/images/lv-shot.jpg" alt=" Lorenzo Vasquez" />
                     </div>
                 </div>
-                <div className="lorenzoImage">
-                    <img id="lorenzo" src="/images/lv-shot.jpg" alt=" Lorenzo Vasquez" />
-                </div>
-            </div>
+            </CSSTransition>
 
             <div id="angleDivWhite"></div>
         </main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Main from "../components/Main";
 import Listings from "../components/Listings";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import { CSSTransition } from "react-transition-group";
 
 export default function Index() {
   return (

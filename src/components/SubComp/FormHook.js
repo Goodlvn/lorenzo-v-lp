803e9795f0945081cde64e31f57fe40330092ca6
JD Martinez <focus4ursoul@gmail.com>
2021-02-24 11:53:06 -0800

    mailchimp form + styling - need client validation

--- src/components/SubComp/FormHook.js
+++ src/components/SubComp/FormHook.js
@@ -37,28 +37,30 @@ export default function FormHook({ status, invalidName, invalidEmail, invalidNum
         //     </form>
         // </>
 
-        <div id="mc_embed_signup">
+        <div id="mc_embed_signup" className="contactForm">
             <form action="https://world.us1.list-manage.com/subscribe/post?u=b931a098fad1826a4bfa44074&amp;id=886f33c92d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
                 <div id="mc_embed_signup_scroll">
-
-                    <div className="indicates-required"><span className="asterisk"></span> indicates required</div>
                     <div className="mc-field-group">
-                        <label htmlFor="mce-FNAME">First Name </label>
+                        {/* <label htmlFor="mce-FNAME">First Name </label> */}
                         <input type="text" name="FNAME" className="" id="mce-FNAME" placeholder="test" />
                     </div>
                     <div className="mc-field-group">
-                        <label htmlFor="mce-LNAME">Last Name </label>
+                        {/* <label htmlFor="mce-LNAME">Last Name </label> */}
                         <input type="text" name="LNAME" className="" id="mce-LNAME" />
                     </div>
                     <div className="mc-field-group">
-                        <label htmlFor="mce-EMAIL">Email Address  <span className="asterisk"></span>
-                        </label>
+                        {/* <label htmlFor="mce-EMAIL">Email Address  <span className="asterisk"></span>
+                        </label> */}
                         <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" />
                     </div>
-                    <div className="mc-field-group input-group">
-                        <strong>Untitled </strong>
-                        <ul><li><input type="radio" value="Buying" name="MMERGE6" id="mce-MMERGE6-0" /><label htmlFor="mce-MMERGE6-0">Buying</label></li>
-                            <li><input type="radio" value="Selling" name="MMERGE6" id="mce-MMERGE6-1" /><label htmlFor="mce-MMERGE6-1">Selling</label></li>
+                    <div className="mc-field-group">
+                        {/* <label htmlFor="mce-PHONE">Phone #  <span className="asterisk"></span>
+                        </label> */}
+                        <input type="phone" name="PHONE" className="required phone" id="mce-PHONE" />
+                    </div>
+                    <div className="mc-field-group input-group contactTypeBtns">
+                        <ul><li><input type="radio" value="Buying" name="MMERGE6" id="mce-MMERGE6-0" /><label htmlFor="mce-MMERGE6-0">BUYING</label></li>
+                            <li><input type="radio" value="Selling" name="MMERGE6" id="mce-MMERGE6-1" /><label htmlFor="mce-MMERGE6-1">SELLING</label></li>
                         </ul>
                     </div>
                     <div id="mce-responses" className="clear">
@@ -66,9 +68,9 @@ export default function FormHook({ status, invalidName, invalidEmail, invalidNum
                         <div className="response" id="mce-success-response" style={{ display: "none" }}></div>
                     </div>
                     <div aria-hidden="true"><input type="text" name="b_0bf1fbd4e855f6b3e48df6e8e_623900f9d2" tabIndex="-1" style={{ visibility: "hidden" }} /></div>
-                    <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></div>
+                    <div className="clear contactSubmitBtn"><input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button contactSubmitBtnInner" /></div>
                 </div>
             </form>
-        </div>
+        </div >
     )
 }

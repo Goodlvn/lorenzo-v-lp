1d088810efba1c40f25625b9e740606a9e380a52
JD Martinez <focus4ursoul@gmail.com>
2021-02-26 12:18:22 -0800

    input form mobile test

--- src/components/SubComp/FormHook.js
+++ src/components/SubComp/FormHook.js
@@ -5,16 +5,16 @@ export default function FormHook({ status, invalidFNAME, invalidLNAME, invalidEM
             <form action="https://world.us1.list-manage.com/subscribe/post?u=b931a098fad1826a4bfa44074&amp;id=886f33c92d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate autoComplete="chrome-off">
                 <div id="mc_embed_signup_scroll">
                     <div className="mc-field-group">
-                        <input type="text" name="FNAME" className={invalidFNAME ? "error" : ""} id="mce-FNAME" placeholder="FIRST NAME" onChange={handleChange} readOnly onFocus={(e) => { console.log(e.target.removeAttribute("readonly")); }} />
+                        <input type="text" name="FNAME" className={invalidFNAME ? "error" : ""} id="mce-FNAME" placeholder="FIRST NAME" onChange={handleChange} />
                     </div>
                     <div className="mc-field-group">
-                        <input type="text" name="LNAME" className={invalidLNAME ? "error" : ""} id="mce-LNAME" placeholder="LAST NAME" onChange={handleChange} readOnly onFocus={(e) => { console.log(e.target.removeAttribute("readonly")); }} />
+                        <input type="text" name="LNAME" className={invalidLNAME ? "error" : ""} id="mce-LNAME" placeholder="LAST NAME" onChange={handleChange} />
                     </div>
                     <div className="mc-field-group">
-                        <input type="phone" name="PHONE" className={invalidPHONE ? "error required phone" : "required phone"} id="mce-PHONE" placeholder="PHONE #" onChange={handleChange} readOnly onFocus={(e) => { console.log(e.target.removeAttribute("readonly")); }} />
+                        <input type="phone" name="PHONE" className={invalidPHONE ? "error required phone" : "required phone"} id="mce-PHONE" placeholder="PHONE #" onChange={handleChange} />
                     </div>
                     <div className="mc-field-group">
-                        <input type="email" name="EMAIL" className={invalidEMAIL ? "error required email" : "required email"} id="mce-EMAIL" placeholder="EMAIL" onChange={handleChange} readOnly onFocus={(e) => { console.log(e.target.removeAttribute("readonly")); }} />
+                        <input type="email" name="EMAIL" className={invalidEMAIL ? "error required email" : "required email"} id="mce-EMAIL" placeholder="EMAIL" onChange={handleChange} />
                     </div>
                     <div className="mc-field-group input-group contactTypeBtns">
                         <ul><li><input type="radio" value="Buying" name="MMERGE6" id="mce-MMERGE6-0" /><label htmlFor="mce-MMERGE6-0">BUYING</label></li>
@@ -34,3 +34,6 @@ export default function FormHook({ status, invalidFNAME, invalidLNAME, invalidEM
         </div >
     )
 }
+
+
+// readOnly onFocus={(e) => { console.log(e.target.removeAttribute("readonly")); }}

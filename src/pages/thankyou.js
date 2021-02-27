dc803a85ef55da9edc6b08893a6a1ae58c8566de
JD Martinez <focus4ursoul@gmail.com>
2021-02-27 13:29:03 -0800

    add href to thank you pg btns

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -1,4 +1,11 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+
 export default function ThankYou() {
+
+    const router = useRouter();
+    const modernBroker = "https://lorenzovasquez.modernbroker.com/featured-listing?mode=grid&page=1&sort=RELEVANCE&listingSource=0%2BFeatured%20Listings&zoom=10&threshold=10&timezone=GMT-0800";
 
     return (
         <section className="tyContainer">
@@ -7,9 +14,10 @@ export default function ThankYou() {
             </div>
             <h3 className="tyTagline">Thanks for reaching out! </h3>
             <div className="tyBtnsContainer">
-                <button className="tyBtn">BACK</button>
-                <button className="tyBtn">MORE PROPERTIES</button>
+                <button className="tyBtn" onClick={() => router.push("/")}>BACK</button>
+                <button className="tyBtn" onClick={() => (window.location.href = modernBroker)}>MORE PROPERTIES</button>
             </div>
+
         </section >
     )
 }
